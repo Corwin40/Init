e56c885a0e25c6935aa9fe37c71d49cc214a089f
Xavier BURKE <xavier.burke@openpixl.fr>
2020-03-14 12:24:42 +0100

    Add Less && bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,19 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+import '../css/app.scss';
+// appel à JQuery
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
