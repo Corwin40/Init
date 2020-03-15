89bcdd14e454a9a6c210a457e9d9dceac7918638
Xavier BURKE <xavier.burke@openpixl.fr>
2020-03-15 14:43:39 +0100

    ingtroduct admin.js et app.js

---
+++ assets/js/admin.js
@@ -0,0 +1,10 @@
+import '../css/admin.scss';
+//Appel Bootstrap - JQuery && dépendances
+const $ = require('jquery');
+require('bootstrap');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+const App =() => {
+    return <h1>Partie d'administration en Réact</h1>
+}
