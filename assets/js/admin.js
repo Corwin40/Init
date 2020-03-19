b2b5abf535132bb4d8c7e08254c0bcc6f7552dbd
Xavier BURKE <xavier.burke@openpixl.fr>
2020-03-19 08:41:21 +0100

    Add security on Navbar - error

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,24 +1,35 @@
 import '../css/admin.scss';
 import NavBar from "./components/Navbar";
-import React from "react";
+import React, { useState, useContext} from "react";
 import ReactDOM from "react-dom";
 import HomePage from "./pages/HomePage";
-import {HashRouter, Switch, Route} from "react-router-dom";
+import {HashRouter, Switch, Route, withRouter} from "react-router-dom";
 import UsersPage from "./pages/WebApp/Users/UsersPage";
 import UserPage from "./pages/WebApp/Users/userPage";
 import LoginPage from  "./pages/LoginPage";
 import RegisterForm from "./pages/WebApp/Users/RegisterForm";
+import AuthContext from "./contexts/AuthContext";
+import authAPI from "./services/authAPI";
 
 //Appel Bootstrap - JQuery && dépendances
 const $ = require('jquery');
 require('bootstrap');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+console.log('Hello Webpack Encore! Edit me in assets/js/admin.js');
+
+authAPI.setup();
 
 const Admin = () => {
+
+    const [isAuthenticated, setIsAuthenticated] = useState(
+        authAPI.isAuthenticated()
+    );
+
+    const NavbarWithRouter = withRouter(Navbar);
+
     return (
         <HashRouter>
-            <NavBar/>
+            <NavbarWithRouter/>
 
             <main className="container-fluid pt-5">
                 <Switch>
--- assets/js/components/Navbar.jsx
+++ assets/js/components/Navbar.jsx
@@ -1,7 +1,21 @@
-import React from "react"
+import React, {useContext, useState} from 'react';
+import {Dropdown} from 'react-bootstrap';
+import authAPI from "../services/authAPI";
 import {NavLink} from "react-router-dom";
+import AuthContext from "../contexts/AuthContext";
+import UsersAPI from "../services/WebApp/UsersAPI";
 
-const NavBar =() => {
+const NavBar =({history}) => {
+
+    const {isAuthenticated, setIsAuthenticated} = useContext(AuthContext);
+    const [user, setUser] = useState([]);
+
+    const handleLogout = () => {
+        authAPI.logout();
+        setIsAuthenticated(false);
+        history.push("/login");
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <NavLink className="navbar-brand" to="/">Init</NavLink>
@@ -26,6 +40,48 @@ const NavBar =() => {
                     <li className="nav-item">
                         <button className="btn btn-success">Déconnection</button>
                     </li>
+                </ul>
+
+                <ul className="navbar-nav ml-auto">
+                    {(!isAuthenticated && (
+                        <>
+                            <li className="nav-item">
+                                <NavLink to="/Register" className="nav-link">
+                                    Inscription
+                                </NavLink>
+                            </li>
+                            <li className="navitem">
+                                <NavLink to="/login" className="btn btn-success mr-1">
+                                    Connexion
+                                </NavLink>
+                            </li>
+                        </>
+                    )) || (
+                        <>
+                            <Dropdown>
+                                <Dropdown.Toggle variant="default" id="dropdown-basic">
+                                    Paramètres
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#/Saisons">Saisons</Dropdown.Item>
+                                    <Dropdown.Item href="#/Cotisations">Cotisations</Dropdown.Item>
+                                    <Dropdown.Item href="#/Typecotisations">type de cotisations</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+
+                            <Dropdown>
+                                <Dropdown.Toggle variant="default" id="dropdown-basic">
+                                    Mon Compte
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#/Saisons">Gestion du compte</Dropdown.Item>
+                                    <Dropdown.Item onClick={handleLogout}>Déconnexion</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </>
+                    )}
                 </ul>
             </div>
         </nav>
